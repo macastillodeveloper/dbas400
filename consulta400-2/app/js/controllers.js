f525cc2697a85de59c74666f558cbbdeeb845792
macastillo.developer <macastillo.developer@gmail.com>
2016-04-08 11:59:29 +0000

    modificaciones

--- consulta400-2/app/js/controllers.js
+++ consulta400-2/app/js/controllers.js
@@ -2,7 +2,11 @@
 angular.module('as400App',[])
     .controller('as400AppCtrl', function ($scope, $http) {
     $http.get('data/DECESOS_SDF101.json').success(function (data) {
-            $scope.polizas = data;
+            $scope.SDF101 = data;
+
+
+
+
             });
 });
 
