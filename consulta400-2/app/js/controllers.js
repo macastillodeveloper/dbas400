ca6687c817456a4471ffa8d4cbcca39f416f3bf9
macastillo.developer <macastillo.developer@gmail.com>
2016-04-12 12:10:20 +0000

    modificaciones

--- consulta400-2/app/js/controllers.js
+++ consulta400-2/app/js/controllers.js
@@ -1,8 +1,29 @@
 
 angular.module('as400App',[])
     .controller('as400AppCtrl', function ($scope, $http) {
+
+    // Importamos datos de pólizas.
     $http.get('data/DECESOS_SDF101.json').success(function (data) {
             $scope.SDF101 = data;
+    // Importamos datos de garantías.
+    // Importamos datos de asegurados.
+    // Importamos datos de primas.
+    // Importamos datos de servicios.
+    // Importamos datos de recibos.
+    // Importamos datos de incidencias.
+    // Importamos datos bancarios.
+    // Importamos datos de siniestros.
+
+
+
+
+            // Constantes
+            $scope.EMPRESA = 'MERIDIANO, S.A.'
+
+            // Campos de trabajo que hay que componer
+            $scope.t_canal = '01-AGESMER'; // DCODCA + DNOMCA
+            $scope.t_zona = '03-ANDALUC.ORIENTAL SUR'; // DCODZG + DNOMZG
+
 
 
 
