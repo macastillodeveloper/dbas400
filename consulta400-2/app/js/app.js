40dbc2b32c892a8b955feaccec29290f040a2622
macastillo.developer <macastillo.developer@gmail.com>
2016-04-19 11:34:14 +0200

    modificaciones

--- consulta400-2/app/js/app.js
+++ consulta400-2/app/js/app.js
@@ -1,4 +1,46 @@
-angular.module('as400App', [
-  'as400App.controllers'
-//,'as400App.services'
-]);
+
+var as400App = angular.module('as400App', ['ngRoute'])
+
+as400App
+
+    .config(function($routeProvider){
+
+        $routeProvider
+
+            .when('/', {
+               controller: 'as400AppCtrl',
+               /*controllerAs: 'vm',*/
+               templateUrl: "inicio.html"
+            })
+
+            .when('/consultas', {
+                controller: 'as400AppCtrl',
+               /* controllerAs: 'vm',*/
+                templateUrl: 'consultas.html'
+            })
+
+            .when("/informes", {
+                controller: "as400AppCtrl",
+                /*controllerAs: 'vm',*/
+                templateUrl: "informes.html"
+            })
+
+            .when("/informes", {
+                controller: "as400AppCtrl",
+                /*controllerAs: 'vm',*/
+                templateUrl: "solicitudes.html"
+            })
+
+            .otherwise({
+                redirectTo: '/'
+      });
+
+    })
+
+/*
+    .controller('appCtrl', function(){
+        //código del controlador
+    });
+*/
+
+
